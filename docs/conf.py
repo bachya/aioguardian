b4de2694fba2a221e501ec7f61341125fd509b3d
Aaron Bach <bachya1208@gmail.com>
2020-04-19 19:12:29 -0600

    Removed incorrect Sphinx key

--- docs/conf.py
+++ docs/conf.py
@@ -59,11 +59,6 @@ html_theme_options = {
     "style_external_links": True
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
 # -- Options for autodoc -------------------------------------------------
 
 autodoc_default_options = {"member-order": "bysource"}
