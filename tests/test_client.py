d66c38bf4a0eb36acbfa88dfc339e1daa3b81cf6
Aaron Bach <bachya1208@gmail.com>
2020-04-26 20:38:40 -0600

    Tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,7 +5,9 @@ from asynctest import CoroutineMock, patch
 import pytest
 
 from aioguardian import Client
-from aioguardian.errors import SocketError
+from aioguardian.errors import CommandError, SocketError
+
+from tests.common import load_fixture
 
 
 @pytest.mark.asyncio
@@ -29,6 +31,20 @@ async def test_connect_timeout():
                 await client.device.ping()
 
         assert str(err.value) == "Connection to device timed out"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "command_response", [load_fixture("ping_success_response.json").encode()]
+)
+async def test_raw_command_success(mock_datagram_client):
+    """Test a successful raw command."""
+    with mock_datagram_client:
+        async with Client("192.168.1.100") as client:
+            ping_response = await client.execute_raw_command(0)
+        assert ping_response["command"] == 0
+        assert ping_response["status"] == "ok"
+        assert ping_response["data"]["uid"] == "ABCDEF123456"
 
 
 @pytest.mark.asyncio
@@ -57,3 +73,14 @@ async def test_unknown_command():
                 await client.device.ping()
 
         assert str(err.value) == "ping command timed out"
+
+
+@pytest.mark.asyncio
+async def test_unknown_raw_command(mock_datagram_client):
+    """Test that an unknown raw command throws an exception."""
+    with mock_datagram_client:
+        with pytest.raises(CommandError) as err:
+            async with Client("192.168.1.100") as client:
+                await client.execute_raw_command(999)
+
+        assert str(err.value) == "Unknown command code: 999"
