12a814fd4f63fbf9ac4ba8ff22749cb9cd0cea7b
Aaron Bach <bachya1208@gmail.com>
2020-05-02 22:15:33 -0600

    Linting

--- aioguardian/errors.py
+++ aioguardian/errors.py
@@ -2,6 +2,14 @@
 from typing import Dict
 
 
+ERROR_CODE_MAPPING: Dict[int, str] = {
+    17: "valve_already_opened",
+    18: "valve_already_closed",
+    19: "valve_already_stopped",
+    20: "valve_moving",
+}
+
+
 class GuardianError(Exception):
     """Define a base error from which all others inherit."""
 
@@ -18,11 +26,3 @@ class SocketError(GuardianError):
     """Define an error related to UDP socket issues."""
 
     pass
-
-
-ERROR_CODE_MAPPING: Dict[int, str] = {
-    17: "valve_already_opened",
-    18: "valve_already_closed",
-    19: "valve_already_stopped",
-    20: "valve_moving",
-}
