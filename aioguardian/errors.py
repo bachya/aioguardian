9368533831760ee5a7a830bcfb77ce99b9dc7eda
Aaron Bach <bachya1208@gmail.com>
2020-05-07 20:41:33 -0600

    Move command error helper to correct module (#46)

--- aioguardian/errors.py
+++ aioguardian/errors.py
@@ -1,5 +1,8 @@
 """Define exception types for ``aioguardian``."""
-from typing import Dict
+from typing import TYPE_CHECKING, Dict
+
+if TYPE_CHECKING:
+    from aioguardian.helpers.command import Command
 
 ERROR_CODE_MAPPING: Dict[int, str] = {
     17: "valve_already_opened",
@@ -25,3 +28,24 @@ class SocketError(GuardianError):
     """Define an error related to UDP socket issues."""
 
     pass
+
+
+def _raise_on_command_error(command: "Command", data: dict) -> None:
+    """Examine a data response and raise errors appropriately.
+
+    :param command: The command that was run
+    :type command: :meth:`aioguardian.helpers.command.Command`
+    :param data: The response data from running the command
+    :type params: ``dict``
+    """
+    if data.get("status") == "ok":
+        return
+
+    # If we know exactly why the command failed, raise that error:
+    if data.get("error_code") in ERROR_CODE_MAPPING:
+        raise CommandError(
+            f"{command.name} command failed: {ERROR_CODE_MAPPING[data['error_code']]}"
+        )
+
+    # Last resort, return a generic error with the response payload:
+    raise CommandError(f"{command.name} command failed (response: {data})")
