5a97172d81b7dd0b1187e9ed7223cd2f56ce1857
Aaron Bach <bachya1208@gmail.com>
2020-07-09 20:26:20 -0600

    Allow None values for system.upgrade_firmware parameters (#57)

--- aioguardian/commands/system.py
+++ aioguardian/commands/system.py
@@ -1,6 +1,6 @@
 """Define system commands."""
 import asyncio
-from typing import Callable, Coroutine
+from typing import Any, Callable, Coroutine, Dict
 
 import voluptuous as vol
 
@@ -8,19 +8,15 @@ from aioguardian.errors import CommandError
 from aioguardian.helpers.command import Command
 import aioguardian.helpers.config_validation as cv
 
-DEFAULT_FIRMWARE_UPGRADE_FILENAME: str = "latest.bin"
-DEFAULT_FIRMWARE_UPGRADE_PORT: int = 443
-DEFAULT_FIRMWARE_UPGRADE_URL: str = "https://repo.guardiancloud.services/gvc/fw"
-
 PARAM_FILENAME: str = "filename"
 PARAM_PORT: str = "port"
 PARAM_URL: str = "url"
 
 UPGRADE_FIRMWARE_PARAM_SCHEMA: vol.Schema = vol.Schema(
     {
-        vol.Required(PARAM_URL): vol.All(cv.url, vol.Length(max=256)),
-        vol.Required(PARAM_PORT): int,
-        vol.Required(PARAM_FILENAME): vol.All(str, vol.Length(max=48)),
+        vol.Optional(PARAM_URL): vol.All(cv.url, vol.Length(max=256)),
+        vol.Optional(PARAM_PORT): int,
+        vol.Optional(PARAM_FILENAME): vol.All(str, vol.Length(max=48)),
     }
 )
 
@@ -95,24 +91,30 @@ class SystemCommands:
     async def upgrade_firmware(
         self,
         *,
-        url: str = DEFAULT_FIRMWARE_UPGRADE_URL,
-        port: int = DEFAULT_FIRMWARE_UPGRADE_PORT,
-        filename: str = DEFAULT_FIRMWARE_UPGRADE_FILENAME,
+        url: str = None,
+        port: int = None,
+        filename: str = None,
         silent: bool = True,
     ) -> dict:
         """Upgrade the firmware on the device.
 
-        :param url: The firmware file's URL
+        :param url: The firmware file's optional URL
         :type url: ``str``
-        :param port: The firmware file's port
+        :param port: The firmware file's optional port
         :type port: ``int``
-        :param filename: The firmware file's filename
+        :param filename: The firmware file's optional filename
         :type filename: ``str``
         :param silent: If ``True``, silence "beep" tones associated with this command
         :type silent: ``bool``
         :rtype: ``dict``
         """
-        params = {PARAM_URL: url, PARAM_PORT: port, PARAM_FILENAME: filename}
+        params: Dict[str, Any] = {}
+        if url:
+            params["url"] = url
+        if port:
+            params["port"] = port
+        if filename:
+            params["filename"] = filename
 
         try:
             UPGRADE_FIRMWARE_PARAM_SCHEMA(params)
