91520c7373ba2455e6a190cedfaba50b31a15670
Aaron Bach <bachya1208@gmail.com>
2021-07-21 13:25:54 -0600

    Enable strict type checking

--- aioguardian/commands/sensor.py
+++ aioguardian/commands/sensor.py
@@ -1,5 +1,5 @@
 """Define sensor commands."""
-from typing import Callable, Coroutine
+from typing import Any, Awaitable, Callable, Dict, cast
 
 import voluptuous as vol
 
@@ -22,20 +22,21 @@ class SensorCommands:
     ``client.sensor``).
     """
 
-    def __init__(self, execute_command: Callable[..., Coroutine]) -> None:
+    def __init__(self, execute_command: Callable[..., Awaitable]) -> None:
         """Initialize."""
-        self._execute_command: Callable[..., Coroutine] = execute_command
+        self._execute_command = execute_command
 
-    async def pair_dump(self, *, silent: bool = True) -> dict:
+    async def pair_dump(self, *, silent: bool = True) -> Dict[str, Any]:
         """Dump information on all paired sensors.
 
         :param silent: If ``True``, silence "beep" tones associated with this command
         :type silent: ``bool``
         :rtype: ``dict``
         """
-        return await self._execute_command(Command.sensor_pair_dump, silent=silent)
+        data = await self._execute_command(Command.sensor_pair_dump, silent=silent)
+        return cast(Dict[str, Any], data)
 
-    async def pair_sensor(self, uid: str, *, silent: bool = True) -> dict:
+    async def pair_sensor(self, uid: str, *, silent: bool = True) -> Dict[str, Any]:
         """Pair a new sensor to the device.
 
         :param uid: The UID of the sensor to pair
@@ -51,11 +52,14 @@ class SensorCommands:
         except vol.Invalid as err:
             raise CommandError(f"Invalid parameters provided: {err}") from None
 
-        return await self._execute_command(
+        data = await self._execute_command(
             Command.sensor_pair_sensor, params=params, silent=silent
         )
+        return cast(Dict[str, Any], data)
 
-    async def paired_sensor_status(self, uid: str, *, silent: bool = True) -> dict:
+    async def paired_sensor_status(
+        self, uid: str, *, silent: bool = True
+    ) -> Dict[str, Any]:
         """Get the status (leak, temperature, etc.) of a paired sensor.
 
         :param uid: The UID of the sensor to pair
@@ -71,11 +75,12 @@ class SensorCommands:
         except vol.Invalid as err:
             raise CommandError(f"Invalid parameters provided: {err}") from None
 
-        return await self._execute_command(
+        data = await self._execute_command(
             Command.sensor_paired_sensor_status, params=params, silent=silent
         )
+        return cast(Dict[str, Any], data)
 
-    async def unpair_sensor(self, uid: str, *, silent: bool = True) -> dict:
+    async def unpair_sensor(self, uid: str, *, silent: bool = True) -> Dict[str, Any]:
         """Unpair a sensor from the device.
 
         :param uid: The UID of the sensor to unpair
@@ -91,6 +96,7 @@ class SensorCommands:
         except vol.Invalid as err:
             raise CommandError(f"Invalid parameters provided: {err}") from None
 
-        return await self._execute_command(
+        data = await self._execute_command(
             Command.sensor_unpair_sensor, params=params, silent=silent
         )
+        return cast(Dict[str, Any], data)
--- aioguardian/commands/valve.py
+++ aioguardian/commands/valve.py
@@ -1,12 +1,12 @@
 """Define valve commands."""
 import logging
-from typing import Callable, Coroutine, Dict
+from typing import Any, Awaitable, Callable, Dict, cast
 
 from aioguardian.helpers.command import Command
 
 _LOGGER = logging.getLogger(__name__)
 
-VALVE_STATE_MAPPING: Dict[int, str] = {
+VALVE_STATE_MAPPING = {
     0: "default",
     1: "start_opening",
     2: "opening",
@@ -31,18 +31,19 @@ class ValveCommands:
     ``client.valve``).
     """
 
-    def __init__(self, execute_command: Callable[..., Coroutine]) -> None:
+    def __init__(self, execute_command: Callable[..., Awaitable]) -> None:
         """Initialize."""
-        self._execute_command: Callable[..., Coroutine] = execute_command
+        self._execute_command = execute_command
 
-    async def close(self) -> dict:
+    async def close(self) -> Dict[str, Any]:
         """Close the valve.
 
         :rtype: ``dict``
         """
-        return await self._execute_command(Command.valve_close)
+        data = await self._execute_command(Command.valve_close)
+        return cast(Dict[str, Any], data)
 
-    async def halt(self) -> dict:
+    async def halt(self) -> Dict[str, Any]:
         """Halt the valve.
 
         Note that calling this method will cause the device to no longer respond to leak
@@ -56,16 +57,18 @@ class ValveCommands:
             "recommended that you call valve_close() or valve_open() as soon as "
             "possible."
         )
-        return await self._execute_command(Command.valve_halt)
+        data = await self._execute_command(Command.valve_halt)
+        return cast(Dict[str, Any], data)
 
-    async def open(self) -> dict:
+    async def open(self) -> Dict[str, Any]:
         """Open the valve.
 
         :rtype: ``dict``
         """
-        return await self._execute_command(Command.valve_open)
+        data = await self._execute_command(Command.valve_open)
+        return cast(Dict[str, Any], data)
 
-    async def reset(self, *, silent: bool = True) -> dict:
+    async def reset(self, *, silent: bool = True) -> Dict[str, Any]:
         """Reset the valve.
 
         This fully resets system motor diagnostics (including open/close count and
@@ -75,9 +78,10 @@ class ValveCommands:
         :type silent: ``bool``
         :rtype: ``dict``
         """
-        return await self._execute_command(Command.valve_reset, silent=silent)
+        data = await self._execute_command(Command.valve_reset, silent=silent)
+        return cast(Dict[str, Any], data)
 
-    async def status(self, *, silent: bool = True) -> dict:
+    async def status(self, *, silent: bool = True) -> Dict[str, Any]:
         """Retrieve status of the valve.
 
         In the payload that is returned, the ``state`` attribute of the valve can be any
@@ -101,6 +105,6 @@ class ValveCommands:
         :type silent: ``bool``
         :rtype: ``dict``
         """
-        resp = await self._execute_command(Command.valve_status, silent=silent)
-        resp["data"]["state"] = VALVE_STATE_MAPPING[resp["data"]["state"]]
-        return resp
+        data = await self._execute_command(Command.valve_status, silent=silent)
+        data["data"]["state"] = VALVE_STATE_MAPPING[data["data"]["state"]]
+        return cast(Dict[str, Any], data)
