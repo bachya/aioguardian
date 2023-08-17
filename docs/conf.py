eda99cb4e0bcabb2613de1585e6a430eaba38d82
Aaron Bach <bachya1208@gmail.com>
2023-08-17 09:26:47 -0600

    Bump version to 2023.08.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = f"{datetime.today().year}, Aaron Bach"
 author = "Aaron Bach"
 
 # The full version, including alpha/beta/rc tags
-release = "2022.10.0"
+release = "2023.08.0"
 
 
 # -- General configuration ---------------------------------------------------
